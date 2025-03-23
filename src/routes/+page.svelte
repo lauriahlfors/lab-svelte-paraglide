19baaacecb9f83bd70edff9ebd2743430f119417
lauriahlfors <lauriahlfors@proton.me>
2025-03-23 23:13:44 +0200

    feat: add ability to switch locales with button press

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { m } from '../paraglide/messages';
+	import { setLocale } from '../paraglide/runtime';
+</script>
+
+<div class="flex w-full flex-col items-center justify-center gap-4 py-24">
+	<div>
+		<button class="rounded-lg bg-neutral-200 px-3 py-1" onclick={() => setLocale('en')}>en</button>
+		<button class="rounded-lg bg-neutral-200 px-3 py-1" onclick={() => setLocale('fi')}>fi</button>
+	</div>
+
+	<h1 class="text-2xl text-neutral-900">{m.greeting_message()}</h1>
+</div>

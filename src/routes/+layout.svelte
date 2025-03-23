e08fd3ba03aae38e3ad091e1777d0eafa56213f0
lauriahlfors <lauriahlfors@proton.me>
2025-03-23 23:00:41 +0200

    feat: add locale anchor tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
 	import '../app.css';
+	import { page } from '$app/state';
+	import { locales, localizeHref } from '../paraglide/runtime';
 
 	let { children } = $props();
 </script>
+
+<div style="display:none">
+	{#each locales as locale (locale)}
+		<a href={localizeHref(page.url.pathname, { locale })}>{locale}</a>
+	{/each}
+</div>
 
 {@render children()}

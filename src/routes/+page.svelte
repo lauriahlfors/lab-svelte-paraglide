6425afaea27cda2f7a4a2f015131060d8ca1aab7
lauriahlfors <lauriahlfors@proton.me>
2025-04-02 09:56:23 +0300

    feat: update layout to showcase paraglide js features

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,57 @@
 <script lang="ts">
 	import { m } from '../paraglide/messages';
 	import { setLocale } from '../paraglide/runtime';
+
+	let dogAmount = $state(0);
 </script>
 
 <div class="flex w-full flex-col items-center justify-center gap-4 py-24">
+	<!-- Locale switcher -->
 	<div>
-		<button class="rounded-lg bg-neutral-200 px-3 py-1" onclick={() => setLocale('en')}>en</button>
-		<button class="rounded-lg bg-neutral-200 px-3 py-1" onclick={() => setLocale('fi')}>fi</button>
+		<button
+			class="rounded-xl border border-neutral-200 bg-neutral-100 px-3 py-2 font-sans shadow-xl"
+			onclick={() => setLocale('en')}>en</button
+		>
+		<button
+			class="rounded-xl border border-neutral-200 bg-neutral-100 px-3 py-2 font-sans shadow-xl"
+			onclick={() => setLocale('fi')}>fi</button
+		>
 	</div>
 
-	<h1 class="text-2xl text-neutral-900">{m.greeting_message()}</h1>
+	<!-- Add or remove dogs -->
+	<div>
+		<button
+			class="rounded-xl border border-neutral-200 bg-neutral-100 px-3 py-2 font-sans shadow-xl"
+			onclick={() => dogAmount++}
+		>
+			{m.dogButtonAdd()}
+		</button>
+		<button
+			class="rounded-xl border border-neutral-200 bg-neutral-100 px-3 py-2 font-sans shadow-xl"
+			onclick={() => dogAmount--}
+		>
+			{m.dogButtonRemove()}
+		</button>
+	</div>
+
+	<!-- Dog count -->
+	<div>
+		<span>{m.dogsAmount({ count: dogAmount })}</span>
+	</div>
+
+	<div class="my-12 flex flex-col items-center justify-center gap-4">
+		<!-- Greeting -->
+		<h1 class="font-sans text-5xl font-medium tracking-tight text-neutral-900">
+			{m.helloWorld({ user: 'Lauri' })}
+		</h1>
+
+		<!-- Dog message -->
+		<h2 class="font-sans text-2xl text-neutral-900">
+			{#if dogAmount < 1}
+				{m.dogsZero()}
+			{:else}
+				{m.dogs({ count: dogAmount })}
+			{/if}
+		</h2>
+	</div>
 </div>
